0a7f29e1efe8f3e812e0c4f3924373da53937a40
Jude-Ugoo <ugooeze0@gmail.com>
2023-12-05 12:25:19 +0100

    fixed comments

--- alx-interview-qus/insert_num_singly_list/13-insert_number.c
+++ alx-interview-qus/insert_num_singly_list/13-insert_number.c
@@ -13,31 +13,31 @@ listint_t * insert_node(listint_t **head, int number)
 {
     listint_t *new_node , *current, *prev;
 
-    // checks if the head pointer is NULL
+    /* Checks if the head pointer is NULL */
     if (!head)
         return NULL;
     
-    // allocate memory for the new node & retuen NULL on allocation fail
+    /* Allocate memory for the new node & retuen NULL on allocation fail */ 
     new_node = malloc(sizeof(listint_t));
     if (!new_node)
         return NULL;
     
-    // Initialize the new node
+    /* Initialize the new node */ 
     new_node->n = number;
     new_node->next = NULL;
 
-    // Initalize current and prev pointers
+    /* Initalize current and prev pointers */
     current = *head;
     prev = NULL;
 
-    // Traverse the list to find the correct position to insert the new node
+    /* Traverse the list to find the correct position to insert the new node */
     while (current && current->n < number)
     {
         prev = current;
         current = current->next;
     }
 
-    // Insert the new node at the beginning of the list
+    /* Insert the new node at the beginning of the list */
     if (!prev)
     {
         new_node->next = *head;
@@ -45,11 +45,11 @@ listint_t * insert_node(listint_t **head, int number)
     }
     else
     {
-        // Insert the new node between prev and current
+        /* Insert the new node between prev and current */
         prev->next = new_node;
         new_node->next = current;
     }
 
-    // Return the address of the new node
+    /* Return the address of the new node */
     return new_node;
 }
